01c5cb9a7e5b0ca5809eae59bbb43c14ea2d43f3
Munkhtemuulen Gantulga <100359699+Xcution43@users.noreply.github.com>
2024-01-02 15:06:26 +0000

    updated database js database:mysql_db

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    database:"ca1db_mysql_table",
+    database:"mysql_db",
     user:"root",
     password:"password"
 
